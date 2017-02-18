e0a7180600c968f12e5968b156b24e61b348e455
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-17 22:15:11 -0700

    accessing search val in state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,14 +2,33 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+// how we initialize state
+// only class components have state, not functional components
+// constructor performs component setup, eg state & var initalization
+// we only use this.state = in constructor
+// Anywhere else use this.setState() to maintain continuity
+  constructor(props){
+    super(props);
+    console.log(super(props));
+    console.log(props);
+    this.state = { term: ''};
+  }
+
   render() {
 // html elements emit change elements when users interact
-    return <input onChange={this.onInputChange}/>;
+    return (
+    <div>
+      <input
+      value={this.state.term};
+      onChange={event => this.setState({ term: event.target.value })}/>;
+    </div>
+    )
   }
 
-  onInputChange() {
+  // onInputChange(event) {
+  //   this.setState({ term: event.target.value })}
+  // }
 
-  }
  }
 
 export default SearchBar;
